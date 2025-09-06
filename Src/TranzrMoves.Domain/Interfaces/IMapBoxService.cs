c73a7e46f1b0a0b0bbfd0c2a62158aa387a8db54
Cloudsty <wi13b051@gmail.com>
2025-09-06 08:35:00 +0100

    feat: added map route

--- Src/TranzrMoves.Domain/Interfaces/IMapBoxService.cs
+++ Src/TranzrMoves.Domain/Interfaces/IMapBoxService.cs
@@ -6,4 +6,31 @@ public interface IMapBoxService
         string originAddressOrPostcode,
         string destAddressOrPostcode,
         bool useTraffic = true);
+    
+    Task<MapRouteDto> GetRouteDataAsync(
+        string originAddress,
+        string destinationAddress,
+        CancellationToken cancellationToken = default);
+}
+
+public class MapRouteDto
+{
+    public List<CoordinateDto> Coordinates { get; set; } = new();
+    public double DistanceMiles { get; set; }
+    public double DurationMinutes { get; set; }
+    public OriginDestinationDto Origin { get; set; } = new();
+    public OriginDestinationDto Destination { get; set; } = new();
+}
+
+public class CoordinateDto
+{
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+}
+
+public class OriginDestinationDto
+{
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+    public string Address { get; set; } = string.Empty;
 }
